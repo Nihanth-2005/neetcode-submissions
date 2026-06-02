16162e6406155a9cbb2486072edda644a28807c3
LAKSHMI NIHANTH VAIDYAM <140785494+Nihanth-2005@users.noreply.github.com>
2026-06-02 20:41:31 +0530

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,17 @@
+import string
+from string import whitespace, punctuation
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i,j=0,len(s)-1
+        #"  No Lemon, no melon"
+        while i<j:
+            while i<j and not s[i].isalnum():
+                i+=1
+            while i<j and not s[j].isalnum():
+                j-=1
+            
+            if s[i].lower()!=s[j].lower():
+                return False
+            
+            i,j=i+1,j-1
+        return True
